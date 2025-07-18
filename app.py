c41bb87d0bcbd12abdd6f9d8351f5f25597017fa
Justin Fox <jfox@een.com>
2025-07-18 08:01:50 -0500

    Fix: bind to 0.0.0.0 and dynamic port for Render

--- app.py
+++ app.py
@@ -46,5 +46,9 @@ def download_markdown():
 
     return send_file(buffer, as_attachment=True, download_name="daily_update.md", mimetype='text/markdown')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
